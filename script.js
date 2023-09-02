b1c2811ddaa023d9061c56945b1e7cf84a29e48d
Jubessin <justinerogun@hotmail.com>
2023-09-02 00:38:43 -0400

    - renamed nav-list-container - added id to nav element - implemented basic navigation show/hide scroll listening functionality

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+let lastScrollY = window.scrollY;
+let nav = document.getElementById('nav');
+
 function sendEmail()
 {
     var link = "mailto:justinerogun@hotmail.com";
     window.location.href = link;
 }
+
+// Hide/Show navigation bar when scrolling.
+window.addEventListener('scroll', function()
+{
+    var scrollY = window.scrollY;
+
+    if (scrollY > lastScrollY)
+    {
+        nav.classList.add('hidden');
+    }
+    else
+    {
+        nav.classList.remove('hidden');
+    }
+    lastScrollY = scrollY;
+});
