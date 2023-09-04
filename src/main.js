148067738717c11bff6fd46724d520b805f8a0c4
Jubessin <justinerogun@hotmail.com>
2023-09-04 19:23:44 -0400

    - removed icons from nav bar - added noselect class to nav element - removed title class - removed window listener - changed default font family - added margin to sections

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { PROJECT_VERSION } from "./constants.js";
 
-let lastScrollY = window.scrollY;
-let nav = document.getElementById('nav');
 let contact_button = document.getElementById('contact-button');
 let project_version = document.getElementById('project-version');
 
@@ -10,22 +8,6 @@ function sendEmail()
     var link = "mailto:justinerogun@hotmail.com";
     window.location.href = link;
 }
-
-// Hide/Show navigation bar when scrolling.
-window.addEventListener('scroll', function()
-{
-    var scrollY = window.scrollY;
-
-    if (scrollY > lastScrollY)
-    {
-        nav.classList.add('hidden');
-    }
-    else
-    {
-        nav.classList.remove('hidden');
-    }
-    lastScrollY = scrollY;
-});
 
 contact_button.onclick = sendEmail;
 
