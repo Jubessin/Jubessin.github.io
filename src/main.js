e5192da8188e6eda473924596289cbdcebb158d9
Jubessin <justinerogun@hotmail.com>
2023-09-04 22:04:02 -0400

    - fix bug with nav element width overlapping - add active style/class for nav links - add listeners for navigation bar link click event - remove title div

--- src/main.js
+++ src/main.js
@@ -12,3 +12,31 @@ function sendEmail()
 contact_button.onclick = sendEmail;
 
 project_version.textContent = PROJECT_VERSION;
+
+function SetupNavigationBar(){
+    const links = document.querySelectorAll('.nav-link');
+
+    if (links.length)
+    {
+        links.forEach((link) => 
+        {
+            link.addEventListener('click', (e) => {
+                if (!link.classList.contains('active')){
+                    links.forEach((link) => {
+                        link.classList.remove('active');
+                    });
+                    link.classList.add('active');
+                }
+            });
+            
+            if (link.href.includes(location.href)){
+                link.classList.add('active');
+            }
+        });
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function(evt)
+{
+    SetupNavigationBar();
+});
