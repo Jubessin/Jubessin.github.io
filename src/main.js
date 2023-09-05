46fbfcb6e61c0052073150c5a094d3502215f12a
Jubessin <justinerogun@hotmail.com>
2023-09-05 01:14:34 -0400

    - fix navigation bar page reloading issue - added experience section class/style - when in rome...

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
 import { PROJECT_VERSION } from "./constants.js";
 
-let contact_button = document.getElementById('contact-button');
-let project_version = document.getElementById('project-version');
-
-function sendEmail()
-{
+function sendEmail(){
+    
     var link = "mailto:justinerogun@hotmail.com";
     window.location.href = link;
 }
 
-contact_button.onclick = sendEmail;
-
-project_version.textContent = PROJECT_VERSION;
-
 function SetupNavigationBar(){
+
     const links = document.querySelectorAll('.nav-link');
 
-    if (links.length)
-    {
+    if (links.length){
         links.forEach((link) => 
         {
             link.addEventListener('click', (e) => {
@@ -28,15 +21,24 @@ function SetupNavigationBar(){
                     link.classList.add('active');
                 }
             });
-            
-            if (link.href.includes(location.href)){
+
+            if (link.href === location.href){
                 link.classList.add('active');
+            }
+            else{
+                link.classList.remove('active');
             }
         });
     }
 }
 
-window.addEventListener('DOMContentLoaded', function(evt)
-{
+window.addEventListener('DOMContentLoaded', function(evt){
+    
     SetupNavigationBar();
+    
+    const contact_button = document.getElementById('contact-button');
+    contact_button.onclick = sendEmail;
+    
+    const project_version = document.getElementById('project-version');
+    project_version.textContent = PROJECT_VERSION;
 });
