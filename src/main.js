333c3c4e2807445f5dc39f6c80d0079203c18b39
Jubessin <justinerogun@hotmail.com>
2023-09-06 13:02:49 -0400

    - implemented basic scrolling fade in animations

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import { PROJECT_VERSION } from "./constants.js";
 import { SetTheme } from "./theme.js";
 import { ToggleTheme } from "./theme.js";
 
+function OnScroll(elements) {
+    elements.forEach(function(element) {
+        if (window.scrollY >= ((element.offsetTop + (element.scrollHeight)) - window.innerHeight)) {
+            element.classList.add("fade-in-up");
+        } else if (window.scrollY < (element.offsetTop - window.innerHeight)) {
+            element.classList.remove("fade-in-up");
+        }
+    });
+}
+
 function SetupNavigationBar() {
 
     const links = document.querySelectorAll('.nav-link');
@@ -10,7 +20,7 @@ function SetupNavigationBar() {
     if (links.length) {
         links.forEach((link) => 
         {
-            link.addEventListener('click', (e) => {
+            link.addEventListener('click', (_) => {
                 if (!link.classList.contains('active')){
                     links.forEach((link) => {
                         link.classList.remove('active');
@@ -33,18 +43,28 @@ function SetupThemeToggle() {
 
     const btn = document.getElementById('theme-btn');
 
-    btn.addEventListener('click', function(e) {
+    btn.addEventListener('click', function(_) {
         ToggleTheme();
     });
 }
 
-window.addEventListener('DOMContentLoaded', function(evt) {
+window.addEventListener('DOMContentLoaded', function(_) {
 
     SetupThemeToggle();
     SetupNavigationBar();
 
     SetTheme(window.localStorage.getItem('theme'));
-
+    
     const project_version = document.getElementById('project-version');
     project_version.textContent = PROJECT_VERSION;
+    
+    const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in-up-on-scroll");
+
+    if (elementsToFadeInUpOnScroll) {
+        window.addEventListener("scroll", function(_) {
+            OnScroll(elementsToFadeInUpOnScroll);
+        });
+
+        OnScroll(elementsToFadeInUpOnScroll);
+    }
 });
