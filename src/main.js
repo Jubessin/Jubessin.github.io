82cc6a8bb3c70634d5ce07e6d3131f2d0b21f0cb
Jubessin <justinerogun@hotmail.com>
2023-09-15 19:22:42 -0400

    - fix issue #49 - add scroll event listener to account for different screen sizes - address issues with projects css styling and responsive screen sizes

--- src/main.js
+++ src/main.js
@@ -7,19 +7,9 @@ import { observeIntersections } from "./observer.js";
 import { startPhotoLoading, stopPhotoLoading } from "./photo-loader.js";
 
 const nav = document.querySelector('.nav-bar');
-const themeToggle = document.querySelector('.theme-btn')
-
-function handleScroll(element, isIntersecting, onVisibleCallback, onInvisibleCallback) {
-
-    if (isIntersecting) {
-        if (onVisibleCallback && window.scrollY >= ((element.offsetTop + (element.scrollHeight)) - window.innerHeight)) {
-            onVisibleCallback();
-        }
-    }
-    else if (onInvisibleCallback && window.scrollY < (element.offsetTop - window.innerHeight)) {
-        onInvisibleCallback();
-    }
-}
+const themeToggle = document.querySelector('.theme-btn');
+const sections = document.getElementsByTagName('section');
+const scrollInFadeUps = document.querySelectorAll('.scroll-fade-in-up');
 
 function setupNavigationBar() {
 
@@ -46,32 +36,89 @@ function setupThemeToggle() {
             .addEventListener('click', toggleTheme);
 }
 
+// https://stackoverflow.com/a/39576399
+function getMostVisible(elements) {
+    var element,
+        viewportHeight = window.innerHeight,
+        max = 0;
+
+    for (var i = 0; i < elements.length; ++i)
+    {
+        let _element = elements.item(i);
+        var visiblePx = getVisibleHeightPx(_element, viewportHeight);
+
+        if (visiblePx > max) {
+            max = visiblePx;
+            element = _element;
+        }
+    }
+
+    return element;
+}
+
+// https://stackoverflow.com/a/39576399
+function getVisibleHeightPx(element, viewportHeight) {
+    var rect = element.getBoundingClientRect(),
+        height = rect.bottom - rect.top,
+        visible = {
+            top: rect.top >= 0 && rect.top < viewportHeight,
+            bottom: rect.bottom > 0 && rect.bottom < viewportHeight
+        },
+        visiblePx = 0;
+
+    if (visible.top && visible.bottom) {
+        // Whole element is visible
+        visiblePx = height;
+    } else if (visible.top) {
+        visiblePx = viewportHeight - rect.top;
+    } else if (visible.bottom) {
+        visiblePx = rect.bottom;
+    } else if (height > viewportHeight && rect.top < 0) {
+        var absTop = Math.abs(rect.top);
+
+        if (absTop < height) {
+            // Part of the element is visible
+            visiblePx = height - absTop;
+        }
+    }
+
+    return visiblePx;
+}
+
+function getVisibleHeightPercent(element, viewportHeight) {
+    var rect = element.getBoundingClientRect(),
+        height = rect.bottom - rect.top,
+        visible = {
+            top: rect.top >= 0 && rect.top < viewportHeight,
+            bottom: rect.bottom > 0 && rect.bottom < viewportHeight
+        },
+        visiblePx = 0;
+
+    if (visible.top && visible.bottom) {
+        // Whole element is visible
+        return 1;
+    } else if (visible.top) {
+        visiblePx = viewportHeight - rect.top;
+    } else if (visible.bottom) {
+        visiblePx = rect.bottom;
+    } else if (height > viewportHeight && rect.top < 0) {
+        var absTop = Math.abs(rect.top);
+
+        if (absTop < height) {
+            // Part of the element is visible
+            visiblePx = height - absTop;
+        }
+    }
+
+    return visiblePx / (isNaN(viewportHeight) ? 1 : viewportHeight);
+}
+
 window.addEventListener('DOMContentLoaded', function(_) {
 
     setupThemeToggle();
     setupNavigationBar();
     
     setTheme(window.localStorage.getItem('theme'));
-
-    observeIntersections(
-        document.querySelectorAll('.scroll-fade-in-up'), 
-        { threshold: [0, 1] }, 
-        (entry) => { 
-            let element = entry.target;
-
-            handleScroll(
-                element, 
-                entry.isIntersecting, 
-                () => element.classList.add('scroll-fade-in-up-active'), 
-                () => element.classList.remove('scroll-fade-in-up-active')); 
-        }, 
-        (element) => { 
-            handleScroll(
-                element, 
-                true, 
-                () => element.classList.add('scroll-fade-in-up-active'), 
-                () => element.classList.remove('scroll-fade-in-up-active')); 
-        });
 
     observeIntersections(
         this.document.querySelectorAll('.landing-image-container'),
@@ -98,28 +145,32 @@ window.addEventListener('DOMContentLoaded', function(_) {
         },
     );
 
-    observeIntersections(
-        this.document.getElementsByTagName('section'),
-        {
-            threshold: [0.6],
-        },
-        (entry) => {
-            let element = entry.target;
-
-            if (!element.id || !entry.isIntersecting)
-                return;
-
-            const links = document.querySelectorAll('.nav-link');
-            
-            links.forEach((link) => {
-                link.classList.toggle('active', link.href.endsWith(element.id));
-            });
-        }
-    );
-
     startPhotoLoading();
 
     const project_version = document.getElementById('project-version');
     project_version.textContent = PROJECT_VERSION;
 });
 
+window.addEventListener('scroll', function(_) {
+
+    let mostVisible = getMostVisible(sections);
+
+    if (mostVisible && mostVisible.id) {
+        const links = document.querySelectorAll('.nav-link');
+        
+        links.forEach((link) => {
+            link.classList.toggle('active', link.href.endsWith(mostVisible.id));
+        });
+    }
+
+    scrollInFadeUps.forEach((element) => {
+        let visiblePercent = getVisibleHeightPercent(element, window.innerHeight);
+
+        if (visiblePercent >= 0.5) {
+            element.classList.add('scroll-fade-in-up-active');
+        }
+        else if (visiblePercent == 0 && window.scrollY < (element.offsetTop - window.innerHeight)) {
+            element.classList.remove('scroll-fade-in-up-active');
+        }
+    })
+});
